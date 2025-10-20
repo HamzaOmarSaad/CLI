cc231c64d5f75255c09da4ab87d126a241f3c8e7
HamzaOmarSaad <hamzaaomar2005@gmail.com>
2025-10-20 16:44:07 +0300

    main flow setup

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+class Parser {
+        String commandName;
+
+        String[] args;
+
+        //This method will divide the input into commandName and args
+        //where "input" is the string command entered by the user
+        public boolean parse(String input){
+            System.out.println("input from user command: " + input);
+            return false;
+        };
+        public String getCommandName(){
+
+            return commandName;
+        }
+
+        public String[] getArgs(){
+            return args;
+        }
+;
+    }
+
+
